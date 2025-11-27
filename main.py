ce6ee30f361852ce40bff26101b377b04d2b075c
Finn Ferchau <ffgm2004@gmail.com>
2025-11-27 11:20:55 +0100

    impl common

---
+++ main.py
@@ -0,0 +1,4 @@
+from nanochat.common import get_base_dir
+
+if __name__ == "__main__":
+    print(get_base_dir())
---
+++ nanochat/common.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+def get_base_dir():
+    load_dotenv()
+    # co-locate nanochat intermediates with other cached data in ~/.cache (by default)
+    if os.environ.get("NANOCHAT_BASE_DIR"):
+        nanochat_dir = os.environ.get("NANOCHAT_BASE_DIR")
+    else:
+        home_dir = os.path.expanduser("~")
+        cache_dir = os.path.join(home_dir, ".cache")
+        nanochat_dir = os.path.join(cache_dir, "nanochat")
+    os.makedirs(nanochat_dir, exist_ok=True)
+    return nanochat_dir
