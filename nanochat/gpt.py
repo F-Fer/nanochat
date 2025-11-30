9f0f2b521b5decdf963d5d7606ca45c7b6666e15
Finn Ferchau <ffgm2004@gmail.com>
2025-11-30 08:57:35 +0100

    finish CausalSelfAttention module

--- nanochat/gpt.py
+++ nanochat/gpt.py
@@ -10,8 +10,8 @@ class GPTConfig:
     sequence_len: int = 1024
     vocab_size: int = 50304
     n_layer: int = 12
-    n_head: int = 6
-    n_kv_head: int = 6
+    n_head: int = 6 # Number of query heads
+    n_kv_head: int = 6 # Number of key/value heads
     n_embd: int = 768
 
 def norm(x):
@@ -21,7 +21,7 @@ def norm(x):
 def apply_rotary_emb(x, cos, sin):
     """
     Apply rotary embeddings to an input.
-    x is of shape (batch_size, num_heads, seq_len, head_dim)
+    x is of shape (batch_size, seq_len, num_heads, head_dim)
     cos and sin are of shape (1, 1, seq_len, head_dim//2)
     """
     assert x.ndim == 4 # Lets say x is of shape (1, 6, 1024, 128)
@@ -33,4 +33,64 @@ def apply_rotary_emb(x, cos, sin):
     return out
 
 class CausalSelfAttention(nn.Module):
-    pass
+    def __init__(self, config: GPTConfig, layer_idx: int) -> None:
+        super().__init__()
+        self.layer_idx = layer_idx
+        self.n_head = config.n_head
+        self.n_kv_head = config.n_kv_head
+        self.n_embd = config.n_embd
+        self.head_dim = self.n_embd // self.n_head
+        assert self.n_embd % self.n_head == 0
+        assert self.n_kv_head <= self.n_head and self.n_head % self.n_kv_head == 0
+        self.c_q = nn.Linear(self.n_embd, self.n_head * self.head_dim, bias=False)
+        self.c_k = nn.Linear(self.n_embd, self.n_kv_head * self.head_dim, bias=False)
+        self.c_v = nn.Linear(self.n_embd, self.n_kv_head * self.head_dim, bias=False)
+        self.c_proj = nn.Linear(self.n_embd, self.n_embd, bias=False)
+
+    def forward(self, x, cos_sin, kv_cache):
+        """
+        Forward the SelfAttention block.
+        x should be of shape (batch_size, seq_len, embd_dim)
+        """
+        B, T, C = x.size()
+
+        # Project the input to get queries, keys, and values
+        q = self.c_q(x).view(B, T, self.n_head, self.head_dim)
+        k = self.c_k(x).view(B, T, self.n_kv_head, self.head_dim)
+        v = self.c_v(x).view(B, T, self.n_kv_head, self.head_dim)
+
+        # Apply rotary embeddings to to queries and keys to get relative positional encoding
+        cos, sin = cos_sin
+        q = norm(apply_rotary_emb(q, cos, sin))
+        k = norm(apply_rotary_emb(k, cos, sin))
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2) # make head be batch dim, i.e. (B, T, H, D) -> (B, H, T, D)
+
+        # Apply KV cache
+        if kv_cache is not None:
+            k, v = kv_cache.insert_kv(self.layer_idx, k, v)
+        Tq = q.size(2) # number of queries in this forward pass
+        Tk = k.size(2) # number of keys/values in total (in the cache + current forward pass)
+
+        enable_gqa = self.n_head != self.n_kv_head # Group Query Attention
+        if kv_cache is None or Tq == Tk:
+            # During training (no kv_cache), attend as usual with causal attention
+            # Even if there is kv cache we van still use this simple version when Tq == Tk
+            y = F.scaled_dot_product_attention(q, k , v, is_causal=True, enable_gqa=enable_gqa)
+        elif Tq == 1:
+            # During inference but with a single query in this forward pass:
+            # The query has to attend to all the keys/values in the cache
+            y = F.scaled_dot_product_attention(q, k , v, is_causal=False, enable_gqa=enable_gqa)
+        else: 
+            # During inference and we have a chunk of queris in this forward pass:
+            # First, each query attends to all the cached keys/values (i.e. full prefix)
+            attn_mask = torch.zeros((Tq, Tk), dtype=torch.bool, device=q.device)
+            prefix_len = Tk - Tq
+            if prefix_len > 0:
+                attn_mask [:, :prefix_len] = True
+            attn_mask[:, prefix_len:] = torch.tril(torch.ones((Tq, Tq), dtype=torch.bool, device=q.device))
+            y = F.scaled_dot_product_attention(q, k , v, attn_mask=attn_mask, enable_gqa=enable_gqa)
+
+        # Re-assemble the heads side by side and project back to residual stream
+        y = y.transpose(1, 2).contiguous().view(B, T, -1)
+        y = self.c_proj(y)
+        return y
